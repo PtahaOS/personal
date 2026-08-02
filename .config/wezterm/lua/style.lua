ca84ab08f8f13fac47006c3246e65b3a8ef12985
PtahaOS <oleksijsorokin7@gmail.com>
2026-08-02 13:02:23 +0300

    waybar rose-pine theme

--- .config/wezterm/lua/style.lua
+++ .config/wezterm/lua/style.lua
@@ -1,15 +1,15 @@
 local wezterm = require("wezterm")
+local rose_pine = require("lua.rose-pine").moon
 local style = {}
 
 -- Font
-style.font = wezterm.font("JetBrains Mono Nerd Font")
+style.font = wezterm.font_with_fallback {
+    "JetBrains Mono Nerd Font",
+   "Iosevka Nerd Font",
+    "Font Awesome 7",
+}
 style.font_size = 20
 
--- Colors
-style.colors = {
-    cursor_bg = "white",
-    cursor_border = "white",
-}
 
 -- Background
 style.window_background_opacity = 0.85
@@ -17,5 +17,9 @@ style.window_background_image = ""
 
 -- Appearance
 style.hide_tab_bar_if_only_one_tab = true
+
+-- Theme
+style.colors = rose_pine.colors()
+style.window_frame = rose_pine.window_frame()
 
 return style
